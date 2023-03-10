de0ad03a4751ecba3e8d03ff11ff2053d9965973
tait smith <taitsmith@protonmail.com>
2023-03-10 07:07:29 -0800

    update tests

--- app/src/test/java/com/taitsmith/schoolio/MainViewModelTest.kt
+++ app/src/test/java/com/taitsmith/schoolio/MainViewModelTest.kt
@@ -15,10 +15,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.Mock
 import org.mockito.Mockito.`when`
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.times
-import org.mockito.kotlin.verify
+import org.mockito.MockitoAnnotations.initMocks
 
 @RunWith(JUnit4::class)
 class MainViewModelTest {
@@ -26,25 +25,36 @@ class MainViewModelTest {
     @get:Rule
     var rule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val mainDispatchRule = MainDispatchRule()
+
+    @Mock
     private lateinit var apiRepository: ApiRepository
 
     private lateinit var viewModel: MainViewModel
 
     private val testDispatcher = TestCoroutineDispatcher()
     private val school1 =  SchoolResponseModel()
-
+    private val scores = SatResponseModel()
     private val mockedSchools = mutableListOf<SchoolResponseModel>()
+    private val mockedScores = mutableListOf<SatResponseModel>()
 
     @Before
     fun setup() {
-        apiRepository = mock()
+        initMocks(this)
 
         school1.dbn = "testSchool"
         school1.schoolName = "cool school"
         school1.borough = "queens"
         school1.overviewParagraph = "stay in school"
-
         mockedSchools.add(0, school1)
+
+        scores.dbn = "testSchool"
+        scores.satWritingAvgScore = "500"
+        scores.satCriticalReadingAvgScore = "500"
+        scores.satMathAvgScore = "500"
+        scores.numOfSatTestTakers = "100"
+        mockedScores.add(0, scores)
 
         viewModel = MainViewModel(apiRepository)
     }
@@ -59,11 +69,23 @@ class MainViewModelTest {
     @Test
     fun `test schools in ViewModel`() = testDispatcher.runBlockingTest {
         `when`(apiRepository.fetchSchools()).thenReturn(mockedSchools)
-
         viewModel.getSchoolList()
+        val returnedSchools = apiRepository.fetchSchools()
+        assertEquals(returnedSchools, viewModel.schools.getOrAwaitValue())
+    }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test empty sat scores set error message in ViewModel`() = testDispatcher.runBlockingTest {
+        `when`(apiRepository.fetchSatScoresForSchool(school1.dbn)).thenThrow(IndexOutOfBoundsException())
+        viewModel.getSatScores(school1)
+        assertEquals("OOB", viewModel.errorMessage.getOrAwaitValue())
+    }
 
-        verify(apiRepository, times(1)).fetchSchools()
-
-        assertEquals(mockedSchools, viewModel.schools.value)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test sat scores in ViewModel`() = testDispatcher.runBlockingTest {
+        `when`(apiRepository.fetchSatScoresForSchool(school1.dbn)).thenReturn(mockedScores)
+        viewModel.getSatScores(school1)
+        assertEquals(mockedScores[0], viewModel.satData.getOrAwaitValue())
     }
 }
