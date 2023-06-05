1580b1389dcccad928fae1529815c80ada6af4dd
tait smith <tait@taitsmith.com>
2023-06-05 09:28:50 -0700

    update text alignment, delete main fragment

--- app/src/main/java/com/taitsmith/schoolio/ui/MainActivity.kt
+++ app/src/main/java/com/taitsmith/schoolio/ui/MainActivity.kt
@@ -1,34 +1,26 @@
 package com.taitsmith.schoolio.ui
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.content.res.Resources.Theme
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.FrameLayout
-import android.widget.Space
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -41,60 +33,35 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.integerArrayResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.snackbar.Snackbar
 import com.taitsmith.schoolio.R
 import com.taitsmith.schoolio.data.SchoolResponseModel
-import com.taitsmith.schoolio.ui.theme.md_theme_dark_primaryContainer
-import com.taitsmith.schoolio.ui.theme.md_theme_dark_tertiary
-import com.taitsmith.schoolio.ui.theme.md_theme_light_onPrimary
 import com.taitsmith.schoolio.ui.theme.md_theme_light_primary
 import com.taitsmith.schoolio.ui.theme.md_theme_light_primaryContainer
 import com.taitsmith.schoolio.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.exp
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
 
-    private var fragmentManager: FragmentManager? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
-        setContentView(R.layout.activity_main)
-        fragmentManager = supportFragmentManager
-        if (savedInstanceState == null) {
-            val fragment = MainFragment()
-            fragmentManager!!.beginTransaction()
-                .replace(R.id.container, fragment)
-                .addToBackStack(null)
-                .commit()
+        setContent{
+            ShowLoading()
         }
         setObservers()
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        if (fragmentManager!!.backStackEntryCount > 0) fragmentManager!!.popBackStack()
-        else super.onBackPressed()
+        viewModel.getSchoolList()
     }
 
     private fun setObservers() {
         viewModel.satData.observe(this) {
-//            val fragment = SchoolInfoFragment(viewModel.selectedSchool, it)
-//            fragmentManager?.beginTransaction()
-//                ?.replace(R.id.container, fragment)
-//                ?.addToBackStack(null)
-//                ?.commit()
+
         }
         viewModel.schools.observe(this) {
             setContent { 
@@ -125,6 +92,29 @@ class MainActivity : AppCompatActivity() {
         //api key for accessing data - would normally hide this in gradle.properties and then access
         //through BuildConfig.appToken or something similar, but to keep things simple we'll leave it here
         var appToken = "m0BLHEdVKLT5GZWpnSVpPvADb"
+    }
+}
+
+@Composable
+fun ShowLoading(){
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = md_theme_light_primary,
+        shadowElevation = 1.dp,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center),
+                color = md_theme_light_primaryContainer
+            )
+        }
     }
 }
 
@@ -193,8 +183,12 @@ fun SchoolCard(school: SchoolResponseModel) {
                                 .fillMaxSize()
                                 .padding(all = 4.dp)
                                 .clickable {
-                                    context.startActivity(Intent(Intent.ACTION_DIAL,
-                                        Uri.parse("tel:${school.phoneNumber}")))
+                                    context.startActivity(
+                                        Intent(
+                                            Intent.ACTION_DIAL,
+                                            Uri.parse("tel:${school.phoneNumber}")
+                                        )
+                                    )
                                 }
                         ) {
                             Image(
@@ -208,7 +202,9 @@ fun SchoolCard(school: SchoolResponseModel) {
                                 text = school.phoneNumber!!,
                                 style = MaterialTheme.typography.bodySmall,
                                 color = md_theme_light_primary,
-                                modifier = Modifier.wrapContentHeight()
+                                modifier = Modifier
+                                    .wrapContentHeight()
+                                    .align(Alignment.CenterVertically)
                             )
                         }
                         Row(
@@ -232,7 +228,9 @@ fun SchoolCard(school: SchoolResponseModel) {
                                 text = school.schoolEmail!!,
                                 style = MaterialTheme.typography.bodySmall,
                                 color = md_theme_light_primary,
-                                modifier = Modifier.wrapContentHeight()
+                                modifier = Modifier
+                                    .wrapContentHeight()
+                                    .align(Alignment.CenterVertically)
                             )
                         }
                     }
